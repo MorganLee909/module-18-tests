36aa7ddf381db559e00d1eb06a14860507ba0c55
Lee Morgan <me@leemorgan.io>
2023-05-07 09:02:59 -0400

    Add options reactions to thought test data creation.

--- testData.js
+++ testData.js
@@ -17,7 +17,7 @@ module.exports = {
         return user;
     },
 
-    createThought: async function(){
+    createThought: async function(reactions = false){
         let user = new User({
             username: faker.internet.userName(),
             email: faker.internet.email(),
@@ -31,6 +31,18 @@ module.exports = {
             createdAt: new Date(),
             reactions: []
         });
+
+        if(reactions){
+            let rand = Math.floor(Math.random() * 5) + 1;
+            for(let i = 0; i < rand; i++){
+                thought.push({
+                    reactionId: new mongoose.Types.ObjectId(),
+                    reactionBody: faker.lorem.sentence(),
+                    username: faker.internet.userName(),
+                    createdAt: new Date()
+                });
+            }
+        }
 
         user.thoughts.push(thought._id);
 
